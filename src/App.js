e47cfce1c8acfdef2297945212364fd676d5895a
kidtzz <bangkitpr4@gmail.com>
2022-02-28 20:09:59 +0700

    hehe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import Navbar1 from "../src/component/Navbar1/Navbar1";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import Navbar1 from "../src/component/Navbar1/Navbar1";
+import Content from "./component/Content/Content";
 
 function App() {
     return (
         <div className="App">
             <Navbar1 />
+            <Content />
+            <Content />
+            <Content />
         </div>
     );
 }
---
+++ src/component/Content/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Content.scss";
+
+const Content = () => {
+    return (
+        <div className="section-content">
+            <div className="container border">
+                <h2>CONTENT</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
